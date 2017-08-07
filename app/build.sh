e4db83fc4e5922fa2fd287cc8f878b000d54a542
ohr486 <ohr486@gmail.com>
2017-08-07 15:02:18 +0900

    improve build.sh

--- app/build.sh
+++ app/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+MIX_EXEC=`which mix`
+
+echo "--- mix_exec: $MIX_EXEC ---"
+
 echo "--- local build app ---"
-MIX_ENV=prod mix deps.get
-MIX_ENV=prod mix compile
+MIX_ENV=prod $MIX_EXEC deps.get
+MIX_ENV=prod $MIX_EXEC compile
