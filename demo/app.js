eef17f35aa3ee852f3f99ba58d8952e663a1c4c8
ohr486 <ohr486@gmail.com>
2017-08-10 17:34:07 +0900

    add demo

---
+++ demo/app.js
@@ -0,0 +1,50 @@
+import {Socket} from "./phoenix"
+
+window.userToken = "YOUR_TOKEN2
+
+// local
+//let socket = new Socket("ws://localhost:4000/socket", {params: {userToken: window.userToken}})
+
+// target
+let socket = new Socket("ws://xxx.xxx.xxx.xxx:4000/socket", {params: {userToken: window.userToken}})
+
+socket.connect()
+socket.onOpen( ev => console.log("OPEN", ev) )
+socket.onError( ev => console.log("ERROR", ev) )
+socket.onClose( ev => console.log("CLOSE", ev) )
+var chan = socket.channel("room:lobby", {})
+
+// Ele
+var $status = $("#status")
+var $messages = $("#messages")
+var $input = $("#single-message-input")
+var $username = $("#username")
+
+// JOIN
+chan.join()
+    .receive("ignore", () => console.log("auth error"))
+    .receive("ok", () => console.log("join ok"))
+    .receive("timeout", () => console.log("Connection interruption"))
+chan.onError(e => console.log("something went wrong", e))
+chan.onClose(e => console.log("channel closed", e))
+
+// INPUT
+$input.off("keypress").on("keypress", e => {
+  if (e.keyCode == 13) {
+    chan.push("new:msg", {user: $username.val(), body: $input.val()}, 10000)
+    $input.val("")
+  }
+})
+
+// REPLY
+chan.on("new:msg", msg => {
+  var body = msg.body;
+  var username = msg.user;
+  var amsg = `<p><a href='#'>[${username}]</a>&nbsp; ${body}</p>`;
+  $messages.append(amsg);
+  scrollTo(0, document.body.scrollHeight)
+})
+chan.on("user:entered", msg => {
+  var username = $("<div/>").text(msg.user || "anonymous").html();
+  $messages.append(`<br/><i>[${username} entered group-chat]</i>`)
+})
